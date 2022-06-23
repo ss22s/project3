38ac210317c1917b9de414fb8251e2828d07d7ca
furukawa <y_ff03242@yahoo.co.jp>
2022-06-23 19:28:43 +0900

    0623

---
+++ Laravel/resources/views/TOP/ranking.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>TOP-ランキング</title>
+</head>
+
+<body>
+    <div>
+        <h1>ランキング</h1>
+    </div>
+    <div>
+        @csrf
+        @for($i = 0; $i < 7; $i++) <div>
+            <!-- 書影 -->
+            <h3>{{$i+1}}位：<a href="">{{$rankingDatas[$i]['book']}}</a></h3>
+            <p>作者：{{$rankingDatas[$i]['auther']}}</p>
+            <p>ジャンル：{{$rankingDatas[$i]['genre']}}</p>
+    </div>
+    @endfor
+    </div>
+</body>
+
+</html>
