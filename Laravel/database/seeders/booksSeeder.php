edf5049fc17356e8e6c65b88b7915af1966c122d
Kanda <st072022@m01.kyoto-kcg.ac.jp>
2022-06-23 10:50:36 +0900

    db_TOTORO+

--- Laravel/database/seeders/booksSeeder.php
+++ Laravel/database/seeders/booksSeeder.php
@@ -26,5 +26,47 @@ class booksSeeder extends Seeder
             'auther' => 'スタジオジブリ',
             'genre' => '児童書'],
         ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1002,
+            'book' => 'カラフル',
+            'auther' => '森絵都',
+            'genre' => '児童書'],
+        ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1003,
+            'book' => 'ハリーポッターと賢者の石',
+            'auther' => 'J・K・ローリング',
+            'genre' => '児童書'],
+        ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1004,
+            'book' => '基本情報技術者過去問題集',
+            'auther' => '技術評論社',
+            'genre' => '問題集'],
+        ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1005,
+            'book' => 'Myojo',
+            'auther' => '集英社',
+            'genre' => '雑誌'],
+        ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1006,
+            'book' => 'カードキャプターさくら',
+            'auther' => 'CLAMP',
+            'genre' => '少女漫画'],
+        ]);
+
+        \DB::table('books')->insert([
+            ['bookID' => 1007,
+            'book' => 'わたしの美しい庭',
+            'auther' => '凪良ゆう',
+            'genre' => '小説・文芸'],
+        ]);
     }
 }
