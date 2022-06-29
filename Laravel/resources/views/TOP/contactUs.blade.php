f8baa33a60509b8273e9a85f0161328c851e298d
st002023 <manyan.sp6@gmail.com>
2022-06-30 02:38:42 +0900

    commit

--- Laravel/resources/views/TOP/contactUs.blade.php
+++ Laravel/resources/views/TOP/contactUs.blade.php
@@ -19,15 +19,15 @@
                         <input type="text" name="name" placeholder="例）田中太郎" value="">
                     </div>
                     <div>
-                        <label for="email">E-mail</label>
-                        <input type="text" name="email" placeholder="例）example@example.com" value="">
+                        <label for="email">メールアドレス</label>
+                        <input type="text" name="email" placeholder="例）guest@example.com" value="">
                     </div>
                     <div>
                         <label for="item">お問い合わせの種類</label>
                         <select name="item">
-                            <option value="contact">お問い合わせ</option>
-                            <option value="opinion">ご意見</option>
-                            <option value="others">その他</option>
+                            <option value="お問い合わせ">お問い合わせ</option>
+                            <option value="ご意見">ご意見</option>
+                            <option value="その他">その他</option>
                         </select>
                     </div>
                     <div>
