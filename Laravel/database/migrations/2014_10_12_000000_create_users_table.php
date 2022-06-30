49fc72a21a4cc5f6bbb1324fc7780f525128dbd4
Kanda <st072022@m01.kyoto-kcg.ac.jp>
2022-06-30 12:08:18 +0900

    ignore

--- Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email');
             $table->string('pass');
             $table->integer('exit')->nullable();
-            //a
         });
     }
 
