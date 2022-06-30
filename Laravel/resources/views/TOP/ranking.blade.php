b46fccb9d6b5d9a7af666778839cf56ddffb63ff
furukawa <y_ff03242@yahoo.co.jp>
2022-06-30 12:16:54 +0900

    12:16

---
+++ Laravel/resources/views/TOP/ranking.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>TOP-ランキング</title>
+</head>
+
+<body>
+    <div>
+        <h1>ランキング</h1>
+    </div>
+    <div>
+        @csrf
+        @for($i = 0; $i < 7; $i++) <div>
+            <!-- 書影 -->
+            <!-- 本の詳細ページリンク -->
+            <h4>{{$i+1}}位：<a href="">{{$rankingDatas[$i]['book']}}</a></h4>
+            <p>作者：{{$rankingDatas[$i]['auther']}}</p>
+            <p>ジャンル：{{$rankingDatas[$i]['genre']}}</p>
+    </div>
+    @endfor
+    </div>
+</body>
+
+</html>
---
+++ Laravel/routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('hello');
+});
+
+//Login
+Route::get('/login',function(){
+    return view('login');
+});
+Route::get('/register',function(){
+    return view('register');
+});
+
+//top関連
+
+Route::get('/chatRoom','App\Http\Controllers\TopController@chatroom');
+
+Route::get('/contactUs','App\Http\Controllers\TopController@contactUS');
+
+ROute::get('/newBookReport','App\Http\Controllers\TopController@newBookReport');
+
+Route::get('/ranking','App\Http\Controllers\TopController@ranking');
+
+Route::get('/topMain','App\Http\Controllers\TopController@topMain');
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//お問い合わせ
+Route::get('/contactUs','App\Http\Controllers\TopController@contactUS');
+Route::post('/confirm','App\Http\COntrollers\TopController@confirm');
+
+//マイページ関連
+Route::get('/myPage','App\Http\Controllers\MyPageController@myPage');
+
