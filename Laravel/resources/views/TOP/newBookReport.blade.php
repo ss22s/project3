11c5965a5b667e2c874516df1d21620854b32feb
m-kaho <kuro_0819@icloud.com>
2022-06-30 10:06:38 +0900

    update

--- Laravel/resources/views/TOP/newBookReport.blade.php
+++ Laravel/resources/views/TOP/newBookReport.blade.php
@@ -17,6 +17,7 @@
         <p style="display:inline-block; padding:5px;">【感想】<br>{{$newBookReportData['comment']}}<!--mb_sbstrで字数制限--></p>
         <p style="text-align:right">{{$newBookReportData['userName']}}</p>
         <p style="text-align:right">更新日:{{$newBookReportData['created_at']}}</p>
+        <br>
     </div>
     @endforeach
 </body>
