0bd8a8956b3ec90c5ba947fde1b3bdc9df4dcfbc
Kanda <st072022@m01.kyoto-kcg.ac.jp>
2022-06-30 12:42:34 +0900

    +

--- Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('pass');
             $table->integer('exit')->nullable();
+            //
         });
     }
 
