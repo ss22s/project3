f3c670d2ff03b244265343567ce4f837186aee05
ss22s <my-github-user-name@users.noreply.github.com>
2022-07-07 01:31:53 +0900

    book

--- Laravel/app/Http/Controllers/BookController.php
+++ Laravel/app/Http/Controllers/BookController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Book;
 
 class BookController extends Controller
 {
@@ -10,9 +13,27 @@ class BookController extends Controller
     public function write(){
         return view('bookReportWrite');
     }
-
+    //'reviewID' => 7以降,'UserID'、'bookID'、'evaluation'、'selectedComment'、'comment'、'Open'、'created_at'
     public function register(Request $request){
-        $reportDataAllGet = Request::all();
-        return view('/');
+        $reportDatasGet = $request->only('book','author','finishedDay','evaluation','selectedComment','comment','open');
+        
+        $bookID = Book::where('book',$reportDatasGet['book'])->value('bookID');
+
+        //登録
+        //reviewは自動加算？userはログイン情報sessionからもらう
+        DB::table('bookReports')->insert([
+            'reviewID' => "",
+            'userID' => "",
+            'bookID' => $bookID,
+            'evaluation' => $reportDatasGet['evaluation'],
+            "selectedComment" => $reportDatasGet['selectedComment'],
+            "comment" => $reportDatasGet['comment'],
+            "Open" => $reportDatasGet['open'],
+            'created_at' =>"",
+        ]);
+
+        //入った場所に返す
+        //eturn redirect('home')->with('result', '感想の登録の成功しました！');
+        return back();
     }
 }
--- Laravel/resources/views/bookReportWrite.blade.php
+++ Laravel/resources/views/bookReportWrite.blade.php
@@ -7,7 +7,8 @@
         本の著者（後で自動化）:author
         読んだ日：finishedDay
         評価：evaluation
-        ひとこと感想:select itemのvalueを0~の数字に
+        ひとこと感想:selectで"selectedComment"
+             itemのvalueを0~の数字に
             例：<option value="0">感動した</option>
         コメント：comment
         公開・非公開：ラジオボタンのnameはopen、
