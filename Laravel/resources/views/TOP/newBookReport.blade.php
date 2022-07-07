e084ed5697638f95c243811ad784e11f9abb4c17
m-kaho <kuro_0819@icloud.com>
2022-07-07 16:40:42 +0900

    update

--- Laravel/resources/views/TOP/newBookReport.blade.php
+++ Laravel/resources/views/TOP/newBookReport.blade.php
@@ -17,11 +17,7 @@
             @break
         @else
             <div style="display:inline-block; border:2px solid; border-radius:30px; text-align:center; margin:20px; padding:10px; width:450px;">
-<<<<<<< HEAD
                     <a href="{{route('book.detail',['bookID'=>$bookData['bookID']])}}"><h2 style="text-align:center">{{$bookData['book']}}</h2></a>
-=======
-                    <a href="{{route('book.detail',['bookID'=>'bookData'])}}"><h2 style="text-align:center">{{$bookData['book']}}</h2></a>
->>>>>>> 99b8d350c786b1ac8ee5e5588f8b2958cc308fae
                     <span style="display:inline-block; border:1px solid; font-size:50px; margin:20px; padding:40px;">書影</span>
                     <!-- <img src = "書籍画像"> -->
                     <span style="border:1px solid; font-size:20px; border-radius:50px; margin:10px; padding:20px;">icon</span> 
