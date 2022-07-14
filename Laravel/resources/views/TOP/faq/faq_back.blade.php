a7b753d74b564171d6ce026dbd629f9023201b7f
st002023 <manyan.sp6@gmail.com>
2022-07-14 11:37:51 +0900

    commit

---
+++ Laravel/resources/views/TOP/faq/faq_back.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>よくあるご質問</title>
+</head>
+<body>
+    <h1>よくあるご質問</h1>
+    <div>
+        <h4>新規登録について</h4>
+        <a href="">
+            <span class="icon--q">Q.</span>
+            <div class="que">質問質問質問質問質問？</div>
+        </a>
+        <a href="">
+            <span class="icon--q">Q.</span>
+            <div class="que">質問質問質問質問質問？</div>
+        </a>
+    </div>
+    <div>
+        <h4>ログインについて</h4>
+        <a href="">
+            <span class="icon--q">Q.</span>
+            <div class="que">質問質問質問質問質問？</div>
+        </a>
+        <a href="">
+            <span class="icon--q">Q.</span>
+            <div class="que">質問質問質問質問質問？</div>
+        </a>
+    </div>
+</body>
+</html>
