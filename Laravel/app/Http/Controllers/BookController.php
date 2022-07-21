d1a9068a0d94eab71c25732f6bc641e4eb060113
ss22s <my-github-user-name@users.noreply.github.com>
2022-07-21 12:47:29 +0900

    0721

--- Laravel/app/Http/Controllers/BookController.php
+++ Laravel/app/Http/Controllers/BookController.php
@@ -14,12 +14,26 @@ class BookController extends Controller
 {
     public function detail($bookID)
     {
-        //TODO:本データを外部から取る場合、書き換え
-        $bookDataGet = Book::where('bookID',$bookID)->first();
-        //ひとこと感想TOP3
-
+        $x = 0;
+        //数加算
+        $selectedCommentsCount = array_fill(0, 10, 0);
         
-        dd($bookDataGet);
+        //TODO:本データを外部から取る場合、書き換え
+        $bookData = Book::where('bookID', $bookID)->first();
+        //TODO:ひとこと感想TOP3.やり方が綺麗でない
+        $selectedCommentsGet = bookReport::selectraw('selectedComment')->where('bookID', $bookID)->get();
+        foreach ($selectedCommentsGet as $selectedCommentGet) {
+            $cnt = 0;
+            $selectedCommentsExplode = explode('/', $selectedCommentGet['selectedComment']);
+            
+            foreach ($selectedCommentsExplode as $commentNum) {
+                
+                $selectedCommentsCount[$commentNum]++;
+            }
+            // rsort($selectedCommentsCount);
+        }
+
+        dd($selectedCommentsCount);
 
         return view('TOP/bookDetail');
     }
@@ -33,33 +47,35 @@ class BookController extends Controller
     //'reviewID' => 7以降,'UserID'、'bookID'、'evaluation'、'selectedComment'、'comment'、'Open'、'created_at'
     public function register(Request $request)
     {
+        //渡すメッセージ
+        $message = "";
         $reportDatasGet = $request->only('book', 'finishedDate', 'evaluation',  'comment', 'open');
         $bookID = Book::where('book', $reportDatasGet['book'])->value('bookID');
         //selectedComment
         $selectedCommentGet = $request->input('selectedComment');
-        $selectedComment = implode(',',$selectedCommentGet);
-        
+        $selectedComment = implode(',', $selectedCommentGet);
+
         //created_atの日付
-        $today = date("Y-m-d");
+        $today = date("Y-m-d H:i:s");
         //user情報
         $userData = Auth::user();
 
         //登録：新着感想
         //reviewは自動加算.userはログイン情報sessionからもらう
-        if (!(DB::table('bookReports')->where('id', $userData['id'])->where('bookID', $bookID)->exists())) {
-            DB::table('bookReports')->insert([
-                'id' => $userData['id'],
-                'bookID' => $bookID,
-                'evaluation' => $reportDatasGet['evaluation'],
-                "selectedComment" =>  $selectedComment,
-                "comment" => $reportDatasGet['comment'],
-                "Open" => $reportDatasGet['open'],
-                "created_at" => $today
-            ]);
+
+        DB::table('bookReports')->insert([
+            'id' => $userData['id'],
+            'bookID' => $bookID,
+            'evaluation' => $reportDatasGet['evaluation'],
+            "selectedComment" =>  $selectedComment,
+            "comment" => $reportDatasGet['comment'],
+            "Open" => $reportDatasGet['open'],
+            "created_at" => $today
+        ]);
 
 
-            $reviewIDget = bookReport::where('id', $userData['id'])->where('bookID', $bookID)->value('reviewID');
-
+        $reviewIDget = bookReport::where('id', $userData['id'])->where('bookID', $bookID)->value('reviewID');
+        if (!(DB::table('finishedBooks')->where('id', $userData['id'])->where('bookID', $bookID)->exists())) {
             //登録：読んだ本
             DB::table('finishedBooks')->insert([
                 'id' => $userData['id'],
@@ -68,9 +84,11 @@ class BookController extends Controller
                 'reviewID' => $reviewIDget,
             ]);
         }
-        DB::table('wantToBooks')->where('id', $userData['id'])->where('bookID', $bookID)->update([
-            'finished' => 1,
-        ]);
+        if ((DB::table('wantToBooks')->where('id', $userData['id'])->where('bookID', $bookID)->exists())) {
+            DB::table('wantToBooks')->where('id', $userData['id'])->where('bookID', $bookID)->update([
+                'finished' => 1,
+            ]);
+        }
 
         //入った場所に返す
         //eturn redirect('home')->with('result', '感想の登録の成功しました！');
