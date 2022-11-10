cce484714337a3cfa680643eb86d2885bbbdb15e
m-kaho <kuro_0819@icloud.com>
2022-11-10 15:36:27 +0900

    update

--- Laravel/resources/views/hello.blade.php
+++ Laravel/resources/views/hello.blade.php
@@ -1,60 +1,50 @@
-<link rel="stylesheet" type="text/css" href="css/hello.css">
-<div class="main">
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-    *{
-        background-color: #faf9f8;
-    }
-    div{
-        background-color: #FFFFFF	;
-        margin: 30px;
-    }
-    </style>
+    <link rel="stylesheet" type="text/css" href="css/hello.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="main">
+        <div class="mypage">
+            <a class="mypagelink" href="/myPage">マイページ</a>
+        </div>
+        <div class="login">
+            @guest
+            <a class="loginlink" href="/login">ログイン</a><br>
+            <a class="loginlink" href="/register">新規登録</a>
+            @endguest
+            @auth
+        </div>
+        <div class="logout">
+            <p class="message">
+                ログイン済み<br>
+                <!--ここのログアウト-->
+                ようこそ、{{Auth::user()->name}}<br>
+            </p>
+            <a class="logoutlink" href={{ route('logout') }} onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                ログアウト
+            </a>
+            <form id='logout-form' action={{ route('logout')}} method="POST" style="display: none;">
+                @csrf
+            @endauth
+        </div>
 
-    <div class="mypage">
-        <a class="mypagelink" href="/myPage">マイページ</a>
-    </div>
-    <div class="login">
-        @guest
-        <a class="loginlink" href="/login">ログイン</a><br>
-        <a class="loginlink" href="/register">新規登録</a>
-        @endguest
-        @auth
-    </div>
-    <div class="logout">
-        <p class="message">
-            ログイン済み<br>
-            <!--ここのログアウト-->
-            ようこそ、{{Auth::user()->name}}<br>
-        </p>
-        <a class="logoutlink" href={{ route('logout') }} onclick="event.preventDefault();
-            document.getElementById('logout-form').submit();">
-            ログアウト
-        </a>
-        <form id='logout-form' action={{ route('logout')}} method="POST" style="display: none;">
-            @csrf
-        @endauth
-    </div>
-
-    <div class="toppage">
+        <div class="toppage">
+            <hr class="line">
+            <a class="toppagelink" href="/">Top</a><br>
+            <a class="toppagelink" href="/ranking">ランキング</a><br>
+            <a class="toppagelink" href="/newBookReport">新着感想</a><br>  
+            <a class="toppagelink" href="/chatRoom">掲示板</a><br>
+            <a class="toppagelink" href="/contactUs">お問い合わせ</a>
+        </div>
         <hr class="line">
-        <a class="toppagelink" href="/">Top</a><br>
-        <a class="toppagelink" href="/ranking">ランキング</a><br>
-        <a class="toppagelink" href="/newBookReport">新着感想</a><br>  
-        <a class="toppagelink" href="/chatRoom">掲示板</a><br>
-        <a class="toppagelink" href="/contactUs">お問い合わせ</a>
+        <div>
+            <a class="toppagelink" href="/reportWrite">感想を書く</a>
+        </div>
     </div>
-    <hr class="line">
-    <div>
-        <a class="toppagelink" href="/reportWrite">感想を書く</a>
-    </div>
-</div>
 </body>
 </html>
