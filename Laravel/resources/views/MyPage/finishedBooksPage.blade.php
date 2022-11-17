73d0bf5e173528741cd14eafbf18ecbdd4047dbf
furukawa <y_ff03242@yahoo.co.jp>
2022-11-17 13:46:31 +0900

    furukawa

--- Laravel/resources/views/MyPage/finishedBooksPage.blade.php
+++ Laravel/resources/views/MyPage/finishedBooksPage.blade.php
@@ -11,66 +11,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
-
+    <link rel="stylesheet" type="text/css" href="css/finishedBooksPage.css">
     <title>読んだ本リスト</title>
-    <style type="text/css">
-        .open {
-            cursor: pointer;
-            /* マウスオーバーでカーソルの形状を変えることで、クリックできる要素だとわかりやすいように */
-        }
-
-        #pop-up {
-            display: none;
-            /* label でコントロールするので input は非表示に */
-        }
-
-        .overlay {
-            display: none;
-            /* input にチェックが入るまでは非表示に */
-        }
-
-        #pop-up:checked+.overlay {
-            display: block;
-            z-index: 9999;
-            background-color: #00000070;
-            position: fixed;
-            width: 100%;
-            height: 100vh;
-            top: 0;
-            left: 0;
-        }
-
-        .window {
-            width: 90vw;
-            max-width: 380px;
-            height: 240px;
-            background-color: #ffffff;
-            border-radius: 6px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-        }
-
-        .text {
-            font-size: 18px;
-            margin: 0;
-        }
-
-        .close {
-            cursor: pointer;
-            position: absolute;
-            top: 4px;
-            right: 4px;
-            font-size: 20px;
-        }
-    </style>
 </head>
 
-<body>
+<body class="top">
     {{-- TODO:読んだ本リストのページ --}}
     {{-- finishedBooksにデータ入っるのでforeachなどで回すと取り出せる！ 
         感想はまだ書いてないけど、読んだ本リストに追加したものはfor回している中でif(reviewID != null) で判断できる--}}
