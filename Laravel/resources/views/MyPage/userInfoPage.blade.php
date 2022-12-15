415b1cc2d526fbce84e0cdd2b7161ea671903a1d
st002023 <manyan.sp6@gmail.com>
2022-12-15 12:21:51 +0900

    commit

--- Laravel/resources/views/MyPage/userInfoPage.blade.php
+++ Laravel/resources/views/MyPage/userInfoPage.blade.php
@@ -38,7 +38,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">感想</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">ユーザー情報編集</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
