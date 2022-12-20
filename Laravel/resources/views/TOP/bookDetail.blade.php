2e9205785a21c58169a3d736cc48f3c5180532e6
m-kaho <kuro_0819@icloud.com>
2022-12-20 23:13:10 +0900

    update

--- Laravel/resources/views/TOP/bookDetail.blade.php
+++ Laravel/resources/views/TOP/bookDetail.blade.php
@@ -9,46 +9,39 @@
 </head>
 
 <body>
-
     <div class="main">
         <div class="menuBar">
             @include('MenuBar')
         </div>
-        <div class="page">本の詳細ページ<div>
-                <div>
-                    <img src="{{$bookThumbnail}}" alt="書影" width="150" height="200"><br>
-                    タイトル：{{$bookData['book']}} <br>
-                    著者：{{$bookData['author']}} <br>
-                    カテゴリ：{{$bookData['categories']}}
-                </div>
-
-                <div>
-                    <button>
-                        <a href="{{ route('book.wantBookAdd', $bookData['bookID'] )}}">読みたい本リストに追加する</a>
-                    </button>
-                    {{-- TODO:フラッシュメッセージらしいCSS 成功と失敗で分けてもいい alertにするのか--}}
-                    @if(session('Message'))
-                    <div>
-                        {{session('Message')}}
+            <div class="page">本の詳細ページ</div><br>
+                <img src="{{$bookThumbnail}}" alt="書影" width="150" height="200">
+                    <div class="data">    
+                        タイトル：{{$bookData['book']}} <br>
+                        著者：{{$bookData['author']}} <br>
+                        カテゴリ：{{$bookData['categories']}}
                     </div>
-                    @endif
-                </div>
 
-                <hr>
+                <br>
+                <a class="favorite" href="{{ route('book.wantBookAdd', $bookData['bookID'] )}}">読みたい本リストに追加する</a>
+                {{-- TODO:フラッシュメッセージらしいCSS 成功と失敗で分けてもいい alertにするのか--}}
+                @if(session('Message'))
                 <div>
-                    <h4>みんなのひとこと感想TOP3</h4>
-                    @foreach($selectedCommentsTop as $selectedCommentTop)
-                    @if($selectedCommentTop['number'] != 0)
-                    {{$selectedCommentTop['comment']}} {{$selectedCommentTop['number']}}人<br>
-                    @endif
-                    @endforeach
+                    {{session('Message')}}
                 </div>
-                <hr>
-                <p>
-                <h4>感想</h4>
-                </p>
-                <h4><a href="/">TOPへ</a> </h4>
-            </div>
-</body>
+                @endif
 
+            <div class="line">
+                <h4>みんなのひとこと感想TOP3</h4>
+                @foreach($selectedCommentsTop as $selectedCommentTop)
+                @if($selectedCommentTop['number'] != 0)
+                {{$selectedCommentTop['comment']}} {{$selectedCommentTop['number']}}人<br>
+                @endif
+                @endforeach
+            </div>
+            <div class="line">
+                <h4>感想</h4>
+            </div>
+            <a class="toppagelink"  href="/">TOPへ</a>
+    </div>
+</body>
 </html>
--- Laravel/resources/views/bookReportWrite.blade.php
+++ Laravel/resources/views/bookReportWrite.blade.php
@@ -1,4 +1,3 @@
-本の感想を書くページ
 <!--
 ・form：
     form actionは"/reportRegister"
@@ -24,66 +23,70 @@
 </head>
 
 <body>
-    <h2>本の感想を書く</h2>
-    <div>
-        <form action="/reportRegister" method="post">
-            @csrf
-            <div>
-                <label>本のタイトル：</label>
-                <input type="hidden" name="bookID" value="{{$bookID}}">
-                <!-- <input type="text" name="book" placeholder="例：となりのトトロ"> -->
-                <b>{{$book}}</b>
-                <button method="get" formaction="/selectBooks">変更する</button>
-            </div>
-            <br>
-            <div>
-                <label>読み終わった日：</label>
-                <input type="date" name="finishedDate">
-            </div>
-            <br>
-            <div>
-                <label>評価：</label>
-                <select name="evaluation">
-                    <option value="0">0</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <br>
-            <div>
-                <label>ひとこと感想（複数選択可）：</label>
+    <div class="main">
+        <div class="menuBar">
+            @include('MenuBar')
+        </div>
+        <div class="page">本の感想を書く</div>
+        <div>
+            <form action="/reportRegister" method="post">
+                @csrf
+                <div>
+                    <label>本のタイトル：</label>
+                    <input type="hidden" name="bookID" value="{{$bookID}}">
+                    <!-- <input type="text" name="book" placeholder="例：となりのトトロ"> -->
+                    <b>{{$book}}</b>
+                    <button method="get" formaction="/selectBooks">変更する</button>
+                </div>
                 <br>
-                <input type="checkbox" name="selectedComment[]" value="0">感動した
-                <input type="checkbox" name="selectedComment[]" value="1">笑った<br>
-                <input type="checkbox" name="selectedComment[]" value="2">面白かった
-                <input type="checkbox" name="selectedComment[]" value="3">怖かった <br>
-                <input type="checkbox" name="selectedComment[]" value="4">ぞくぞくした
-                <input type="checkbox" name="selectedComment[]" value="5">文章が好き <br>
-                <input type="checkbox" name="selectedComment[]" value="6">描写が綺麗
-                <input type="checkbox" name="selectedComment[]" value="7">泣いた <br>
-                <input type="checkbox" name="selectedComment[]" value="8">オススメしたい
-                <input type="checkbox" name="selectedComment[]" value="9">つまらなかった <br>
-            </div>
-            <br>
-            <div>
-                <label>感想：</label>
-                <input type="text" name="comment" maxlength="10000" style="width: 50%;  line-height: 1.5; height: 6em;">
-            </div>
-            <br>
-            <div>
-                <label>公開：</label>
-                <input type="radio" name="open" value="0">公開
-                <input type="radio" name="open" value="1" checked>非公開
-            </div>
-            <br>
-            <div>
-                <button type="submit">登録</button>
-            </div>
-        </form>
+                <div>
+                    <label>読み終わった日：</label>
+                    <input type="date" name="finishedDate">
+                </div>
+                <br>
+                <div>
+                    <label>評価：</label>
+                    <select name="evaluation">
+                        <option value="0">0</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                </div>
+                <br>
+                <div>
+                    <label>ひとこと感想（複数選択可）：</label>
+                    <br>
+                    <input type="checkbox" name="selectedComment[]" value="0">感動した
+                    <input type="checkbox" name="selectedComment[]" value="1">笑った<br>
+                    <input type="checkbox" name="selectedComment[]" value="2">面白かった
+                    <input type="checkbox" name="selectedComment[]" value="3">怖かった <br>
+                    <input type="checkbox" name="selectedComment[]" value="4">ぞくぞくした
+                    <input type="checkbox" name="selectedComment[]" value="5">文章が好き <br>
+                    <input type="checkbox" name="selectedComment[]" value="6">描写が綺麗
+                    <input type="checkbox" name="selectedComment[]" value="7">泣いた <br>
+                    <input type="checkbox" name="selectedComment[]" value="8">オススメしたい
+                    <input type="checkbox" name="selectedComment[]" value="9">つまらなかった <br>
+                </div>
+                <br>
+                <div>
+                    <label>感想：</label>
+                    <input type="text" name="comment" maxlength="10000" style="width: 50%;  line-height: 1.5; height: 6em;">
+                </div>
+                <br>
+                <div>
+                    <label>公開：</label>
+                    <input type="radio" name="open" value="0">公開
+                    <input type="radio" name="open" value="1" checked>非公開
+                </div>
+                <br>
+                <div>
+                    <button type="submit">登録</button>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
-
 </html>
