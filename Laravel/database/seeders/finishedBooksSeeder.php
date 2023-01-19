47c2d24c4483669f5ef2274e98d4ab16881fb478
furukawa <y_ff03242@yahoo.co.jp>
2023-01-19 10:44:01 +0900

    furukawa

--- Laravel/database/seeders/finishedBooksSeeder.php
+++ Laravel/database/seeders/finishedBooksSeeder.php
@@ -21,91 +21,67 @@ class finishedBooksSeeder extends Seeder
         Schema::enableForeignKeyConstraints();
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 1,
-                'id' => 1,
-                'bookID' => 'Y2YPcgAACAAJ',
-                'date' => '2022-01-15 15:25:00',
-                'reviewID' => 1,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'Y2YPcgAACAAJ',
+            'date' => '2022-01-15 15:25:00',
+            'reviewID' => 1,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 2,
-                'id' => 2,
-                'bookID' => 'FA-dDQAAQBAJ',
-                'date' => '2022-02-15 11:54:00',
-                'reviewID' => 3,
-                'delete' => 1
-            ]
+            ['id' => 2,
+            'bookID' => 'FA-dDQAAQBAJ',
+            'date' => '2022-02-15 11:54:00',
+            'reviewID' => 3,
+            'delete' => 1]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 3,
-                'id' => 1,
-                'bookID' => 'FLc9zwEACAAJ',
-                'date' => '2022-03-18 16:24:00',
-                'reviewID' => null,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'FLc9zwEACAAJ',
+            'date' => '2022-03-18 16:24:00',
+            'reviewID' => null,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 4,
-                'id' => 3,
-                'bookID' => 'xMMeNwAACAAJ',
-                'date' => '2022-03-23 13:54:00',
-                'reviewID' => 2,
-                'delete' => null
-            ]
+            ['id' => 3,
+            'bookID' => 'xMMeNwAACAAJ',
+            'date' => '2022-03-23 13:54:00',
+            'reviewID' => 2,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 5,
-                'id' => 1,
-                'bookID' => 'jfApAQAAMAAJ',
-                'date' => '2021-03-18 16:24:00',
-                'reviewID' => null,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'jfApAQAAMAAJ',
+            'date' => '2021-03-18 16:24:00',
+            'reviewID' => null,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 6,
-                'id' => 1,
-                'bookID' => 'xMMeNwAACAAJ',
-                'date' => '2022-05-08 10:20:00',
-                'reviewID' => null,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'xMMeNwAACAAJ',
+            'date' => '2022-05-08 10:20:00',
+            'reviewID' => null,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 7,
-                'id' => 1,
-                'bookID' => 'FccyEAAAQBAJ',
-                'date' => '2020-09-13 12:00:00',
-                'reviewID' => null,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'FccyEAAAQBAJ',
+            'date' => '2020-09-13 12:00:00',
+            'reviewID' => null,
+            'delete' => null]
         ]);
 
         \DB::table('finishedBooks')->insert([
-            [
-                'finishedId' => 8,
-                'id' => 1,
-                'bookID' => 'FA-dDQAAQBAJ',
-                'date' => '2021-12-24 00:00:00',
-                'reviewID' => null,
-                'delete' => null
-            ]
+            ['id' => 1,
+            'bookID' => 'FA-dDQAAQBAJ',
+            'date' => '2021-12-24 00:00:00',
+            'reviewID' => null,
+            'delete' => null]
         ]);
     }
 }
